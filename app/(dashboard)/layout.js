ecfda7de273ee253e59a3f94bc4dcebaa5f1c4c1
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-23 12:52:57 +0100

    updating my loayout.js

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,11 +1,23 @@
 import { auth } from "@/auth";
 import DashboardNavigations from "@/components/dashboard/dashboardNavigations";
+import { BASE_URL } from "@/libs/config/configuration";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function Layout({ children }) {
   const session = await auth();
-  console.log(session);
   if (!session?.user && !session) return redirect("/");
+  setInterval(async () => {
+    const request = await fetch(`${BASE_URL}/api/cors/polls`, {
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: (await cookies()).toString(),
+      },
+    });
+    const response = await request.json();
+    console.log(response);
+  }, 10000);
+
   return (
     <>
       <DashboardNavigations session={session}>{children} </DashboardNavigations>
