1fdbc6f87694806fd322104aa93716d57478edcf
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-23 12:44:28 +0100

    fix: add session check for redirect in Layout component

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,10 +1,11 @@
 import { auth } from "@/auth";
 import DashboardNavigations from "@/components/dashboard/dashboardNavigations";
+import { redirect } from "next/navigation";
 
 export default async function Layout({ children }) {
   const session = await auth();
   console.log(session);
-
+  if (!session?.user && !session) return redirect("/");
   return (
     <>
       <DashboardNavigations session={session}>{children} </DashboardNavigations>
