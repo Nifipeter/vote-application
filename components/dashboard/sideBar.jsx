2ccfa4c591a351c6335ec42579e0794960357135
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-28 14:26:43 +0100

    feat: update SideBar component to include logo and change title to 'Ballot Right'

--- components/dashboard/sideBar.jsx
+++ components/dashboard/sideBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import {
   BarChart3,
   Settings,
@@ -43,15 +44,19 @@ export default function SideBar({
     >
       <div className="flex h-full flex-col">
         <div className="flex items-center gap-3 border-b border-gray-200 dark:border-gray-700 px-6 py-5">
-          <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600 dark:bg-indigo-500 shrink-0">
-            <BarChart3 className="h-6 w-6 text-white" />
+          <div className="flex  items-center justify-center rounded-lg  shrink-0">
+            <img
+              src="/logo-only.png"
+              className="h-12 w-12 rounded-2xl"
+              alt="logo"
+            />
           </div>
           <span
             className={`text-xl font-bold text-gray-800 dark:text-white whitespace-nowrap ${
               sidebarCollapsed ? "lg:hidden" : ""
             }`}
           >
-            VoteApp
+            Ballot Right
           </span>
         </div>
 
